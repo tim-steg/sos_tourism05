d6faf22a66496eac4f305cd31e06ca465569be7a
Tim Stegall <47236753+tim-steg@users.noreply.github.com>
2020-07-08 14:48:32 -0500

    Stuff

--- index.php
+++ index.php
@@ -1,1 +1,107 @@
-<?php header('Location: ./src/index.html');  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Insert info about the company.">
+
+    <meta name="keywords" content="events, finding events, covid-19, safety, plan an event">
+
+    <link rel="icon" href="../res/favicon.ico" type="image/x-icon">
+
+    <title>Logo Name Company</title>
+    <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</head>
+
+<body>
+    <section class="home-bg">
+        <header>
+            <a href="index.html"><img src="../res/logo.png" alt="Logo of Logo Name Company."></a>
+
+            <nav>
+                <input type="checkbox" id="check">
+                <label for="check" class="checkbtn">
+                    <i class="fas fa-bars"></i>
+                </label>
+                
+                <ul>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Safety</a></li>
+                    <li id="login" class="aba"><a href="#">Sign up</a></li>&nbsp;&nbsp;&nbsp;&nbsp; |
+                    <li id="login" class="bab"><a href="#">Log in</a></li>
+                </ul>
+            </nav>
+
+        </header>
+
+        <div class="event-bg">
+            <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id dictum velit, non rutrum tortor. Phasellus hendrerit erat dignissim lectus euismod dictum.</p>
+
+            <div class="search-box">
+                <form action="">
+                <label for="search events"></label>
+                <input type="text" name="search events" id="search events" value="What event are you looking for?" size="20"
+                onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
+                
+
+                <input type="button" value="Search" id="find-button">
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <section class="circle-box">
+        <div class="circle-1"> 
+            <div class="circle-text1">
+            <h2>Plan your event.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id dictum velit, non rutrum tortor.Integer pellentesque augue nec tempor euismod. Praesent congue hendrerit urna ac ultricies. </p>
+            </div>
+
+            <div><img src="../res/laptop.png" alt="Image of a laptop"></div>
+
+           <div><input type="button" value="Get Started" id="get-button"></div>
+        </div>
+
+        <div class="circle-2"> 
+            <div class="circle-text2">
+            <h2>Stay safe at a distance.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id dictum velit, non rutrum tortor. Integer pellentesque augue nec tempor euismod. Praesent congue hendrerit urna ac ultricies. </p>
+            </div>
+
+            <div><img src="../res/talk.png" alt="Image of a person and a speech bubble."></div>
+        </div>
+
+        <div class="circle-3"> 
+            <div class="circle-text3"> 
+            <h2>Have a good time.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id dictum velit, non rutrum tortor. Integer pellentesque augue nec tempor euismod. Praesent congue hendrerit urna ac ultricies.</p>
+            </div>
+
+            <div><img src="../res/headphones.png" alt="Image of a pair of headphones."></div>
+        </div>
+    </section>
+
+    <div class="start-bg">
+        <h1>Start planning now.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id dictum velit, non rutrum tortor. Integer pellentesque augue nec tempor euismod. Praesent congue hendrerit urna ac ultricies.</p>
+
+        <input type="button" value="Get Started" id="start-button">
+    </div>
+
+    <footer>
+        <ul>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Safety</a></li>
+            <li><a href="#">Terms</a></li>
+            <li id="login"><a href="#">Sign up</a></li>&nbsp;&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;&nbsp
+            <li id="login" class="ada"><a href="#">Log in</a></li>
+        </ul>
+        <!--(&nbsp;&nbsp;) adds a space between words.-->
+        <a href="index.html"><img src="../res/logo.png" alt="Logo of Logo Name Company."></a>
+    </footer>
+</body>
+</html>
