08fcfab7a46db650bcb402512cac35741ce7becc
Tim Stegall <47236753+tim-steg@users.noreply.github.com>
2020-07-11 15:23:16 -0500

    Update db.php

--- src/db.php
+++ src/db.php
@@ -2,7 +2,7 @@
     
     // Class to connect to our service's database.
     class dbConnect {
-        private $url, $server, $username, $password, $db;
+        private $url, $server, $username, $password, $db, $conn;
 
         function __construct() {
             $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -14,19 +14,23 @@
 
         // Funtion that creates a new mysqli connection and returns it.
         function connectToDB() {
-            $conn = new mysqli($this->server, $this->username, $this->password, $this->db) or die("Couldn't connect to server.");
+            $this->conn = new mysqli($this->server, $this->username, $this->password, $this->db) or die("Couldn't connect to server.");
             return $conn;
         }
 
         // grabs event data based on the event id.
         function grabEventData($eventid) {
-            $res = $this->query("SELECT * FROM events WHERE eventid=$eventid");
+            $res = $this->conn->query("SELECT * FROM events WHERE eventid=$eventid");
             if ($res) {
                 return $res->fetch_array(MYSQLI_ASSOC);
             } else {
                 // return false on error.
                 return false;
             }
+        }
+        
+        function closeConn() {
+            $this->conn->close();
         }
     }
 ?>
