9c53625ead179c5e1dae987cc3967921009b7916
Tim Stegall <47236753+tim-steg@users.noreply.github.com>
2020-07-15 01:50:52 -0500

    Update create-event.js

--- src/create-event.js
+++ src/create-event.js
@@ -4,13 +4,13 @@
 
     var new_session=`<div class="event-session">
                         <div class="collapsible">
-                            <input type="text" class="editable" name="sessname[]" contenteditable placeholder="Enter Session Name" required>
+                            <input type="text" class="editable" name="sessname[]" contenteditable placeholder="Add Session Name" required>
                             <i class="fa fa-caret-down" aria-hidden="true"></i>
                             <i class="fa fa-caret-up" aria-hidden="true"></i>
                             <i class="far fa-trash-alt"></i>
                         </div>
                         <div class="session-content">
-                            <textarea type="text" name="sessdesc[]" placeholder="enter session info" class="session-info" required></textarea>
+                            <textarea type="text" name="sessdesc[]" placeholder="Enter session info" class="session-info" required></textarea>
                         </div>
                     </div>`   
                     
