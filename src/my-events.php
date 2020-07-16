c37503fbd1284541e20b3d2b3fbd1bae84ded0f2
Tim Stegall <47236753+tim-steg@users.noreply.github.com>
2020-07-16 00:18:27 -0500

    Update my-events.php

--- src/my-events.php
+++ src/my-events.php
@@ -5,7 +5,7 @@
     require_once("db.php");
     session_start();
 
-    if (isset($_SESSION['userauth'])) {
+    if (isset($_SESSION['authuser'])) {
         $dbcon = new dbConnect();
         $dbcon->connectToDB();
 
