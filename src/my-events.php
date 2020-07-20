c2c396e6673fe3c48e246db0c4617608da655254
Tim Stegall <47236753+tim-steg@users.noreply.github.com>
2020-07-20 15:36:58 -0500

    Update my-events.php

--- src/my-events.php
+++ src/my-events.php
@@ -51,7 +51,7 @@
             <a class="navlink" href="index.html" id="home">Home</a>
             <a class="navlink" href="">About</a>
             <a class="navlink" href="">Safety</a>
-            <a class="navlink" href="">My Events</a>
+            <?php echo "<a class='navlink' href='./my-events.php?id=".$_SESSION['userid']."'>My Events</a>"; ?>
             <a class="navlink" href="./logout.php"><span></span>Log Out</a>
         </div>
 
