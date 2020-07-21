891b3e22e0cde2df11dfcebbb0b1f457ee595f6f
Tim Stegall <47236753+tim-steg@users.noreply.github.com>
2020-07-20 20:02:58 -0500

    Adding html versions for styling purposes

---
+++ htmlversions/create-event.js
@@ -0,0 +1,91 @@
+
+
+(function() {
+
+    var new_session=`<div class="event-session">
+                        <div class="collapsible">
+                            <input type="text" class="editable" name="sessions[]" contenteditable placeholder="Add Session Name" required>
+                            <i class="fa fa-caret-down" aria-hidden="true"></i>
+                            <i class="fa fa-caret-up" aria-hidden="true"></i>
+                            <i class="far fa-trash-alt"></i>
+                        </div>
+                        <div class="session-content">
+                            <textarea type="text" name="sessions[]" placeholder="Enter session info" class="session-info" required></textarea>
+                        </div>
+                    </div>`   
+                    
+    //Toggle side bar button
+    $(".mobile-toggle").on('click', function(){
+        $(this).toggleClass("mobile-toggle-active")
+        $("nav").toggleClass("nav-active");
+        $(".top-menu").toggleClass("top-menu-active")
+        $(".content").toggleClass("content-active");
+        $(".create,.delete").toggleClass("button-active");
+        $("i.fa-plus, i.fa-minus").toggleClass("icon-active");
+    })
+
+    //Event listener that listens to dynamically addded sessions
+    $(document).delegate('.fa-caret-down,.fa-caret-up', 'click', function(){
+        //console.log(e.keyCode);
+        var collapse=$(this).parent();
+        collapse.toggleClass("active");
+        var content=collapse.next();
+        var fa_up=collapse.find(".fa-caret-up");
+        var fa_down=collapse.find(".fa-caret-down");
+
+        if(content.css("max-height")=="0px"){
+            content.css("max-height", content.prop("scrollHeight")+"px");
+            fa_up.css("display", "block");
+            fa_down.css("display", "none");
+        }
+        else {
+            content.css("max-height", "0px");
+            fa_up.css("display", "none");
+            fa_down.css("display", "block");
+        }
+   });
+
+
+
+   //Event listener that listens to dynamically added sessions
+   $(document).delegate('.fa-trash-alt','click', function () {
+       //console.log("trash");
+       var session=$(this).parent().parent();
+       session.remove();
+
+       //traverse and update number
+       var number=1;
+       //console.log("deleting");
+       $('.event-session').each(function(){
+        //console.log(number);
+        $(this).attr('id', 'session'+number);
+        number++;
+        
+    });
+
+       
+   });
+
+   //Add session event handler
+   $(document).on('click', '.add-session', function(){
+       
+    var pre_session=$('.add-session').prev();
+    //console.log(pre_session);
+    pre_session.after(new_session);
+    //traverse and assign number
+    //console.log("adding");
+    var number=1;
+    $('.event-session').each(function(){
+        //console.log(number);
+        $(this).attr('id', 'session'+number);
+        number++;
+        
+    });
+   })
+   
+})();
+
+// sets minimum date for date inputs.
+var todaydate = new Date().toISOString().split('T')[0];
+document.getElementById("date1").setAttribute('min', todaydate);
+document.getElementById("date2").setAttribute('min', todaydate);
---
+++ htmlversions/db.php
@@ -0,0 +1,185 @@
+<?php
+    
+    // Class to connect to our service's database.
+    class dbConnect {
+        private $url;
+        private $server;
+        private $username;
+        private $password;
+        private $db;
+        private $conn;
+
+        function __construct() {
+            // sets connection parameters
+            $this->url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $this->server = $this->url["host"];
+            $this->username = $this->url["user"];
+            $this->password = $this->url["pass"];
+            $this->db = substr($this->url["path"], 1);
+            $this->conn = NULL;
+        }
+
+        // Funtion that creates a new mysqli connection and returns it.
+        function connectToDB() {
+            $this->conn = new mysqli($this->server, $this->username, $this->password, $this->db) or die("Couldn't connect to server.");
+        }
+
+        // grabs event data based on the event id.
+        function grabEventData($eventid) {
+            $res = $this->conn->query("SELECT * FROM events WHERE eventid=$eventid");
+            if ($res) {
+                return $res->fetch_array(MYSQLI_ASSOC);
+            } else {
+                // return false on error.
+                return false;
+            }
+        }
+        
+        function insertReqs($eventid, $reqs, $attnd1, $attnd2) {
+            // inserts the recommended precautions into its own table.
+            $stmt = $this->conn->prepare("INSERT INTO `reqs` VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("isssisi", $eventid, $reqs[0], $reqs[1], $reqs[2], $attnd1, $reqs[3], $attnd2);
+            $stmt->execute();
+        }
+
+        // inserts all the various session data into a cross-reference table.
+        function insertSessions($eventid, $sessions) {
+            try {
+                $i = 0;
+                $stmt = $this->conn->prepare("INSERT INTO `sessions` VALUES (?, ?, ?)");
+                for ($i = 0; $i < count($sessions[1]); $i+=2) {
+                    $sess1 = $sessions[$i]; $sess2 = $sessions[$i+1];
+                    $stmt->bind_param("iss", $eventid, $sess1, $sess2);
+                    $stmt->execute();
+                }
+            } catch (Exception $e) {
+                die("Exception Error: ".$e->getMessage()."\n");
+            }
+        }
+
+        // function to insert the core data for an event.
+        function insertNewEvent($userid, $eventname, $organizer, $startdate, 
+                                $enddate, $location, $descr, $timezone, $website, $tele, 
+                                $email) {
+            
+            try {
+                $stmt = $this->conn->prepare("INSERT INTO events (`userid`, `eventname`, `organizer`, `startdate`,
+                `enddate`, `location`, `descr`, `timezone`, `website`, `telephone`, `email`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+                $stmt->bind_param("issssssssss", $userid, $eventname, $organizer, $startdate, 
+                                                $enddate, $location, $descr, 
+                                                $timezone, $website, $tele, $email);
+
+                $stmt->execute();
+
+                $eventid = $this->conn->insert_id;
+                return $eventid;
+            } catch (Exception $e) {
+                die("Exception Error: ".$e->getMessage()."\n");
+            }
+        }
+
+        // checks if an account already exists on-signup with the same username or email.
+        function accAlExists($email, $username) {
+            try {
+                $numrows = -1;
+                $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+                $stmt->bind_param("s", $email);
+                $stmt->execute();
+                $stmt->store_result();
+                $numrows = $stmt->num_rows();
+
+                if ($numrows >= 1) {
+                    // account exists, has the same email.
+                    return true;
+                } else if ($numrows <= 0) {
+                    $numrows = -1;
+                    $stmt = $this->conn->prepare("SELECT * FROM users WHERE username = ?");
+                    $stmt->bind_param("s", $username);
+                    $stmt->execute();
+                    $stmt->store_result();
+                    $numrows = $stmt->num_rows;
+
+                    if ($numrows >= 1) {
+                        // has same username
+                        return true;
+                    }
+
+                    // account doesn't exist.
+                    return false;
+                }
+            } catch (Exception $e) {
+                die("Exception Error: ".$e->getMessage()."\n");
+            }
+        }
+
+        // creates a new user entry on the database.
+        function createUser($email, $username, $password) {
+            try {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $stmt = $this->conn->prepare("INSERT INTO users (`email`, `password`, `username`, `status`) VALUES (?, ?, ?, ?)");
+                if ($stmt) {
+                    $num = 1;
+                    $stmt->bind_param("sssi", $email, $hash, $username, $num);
+                    $stmt->execute();
+                }
+            } catch (Exception $e) {
+                die("Exception Error: " .$e->getMessage()."\n");
+            }
+        }
+
+        function closeConn() {
+            $this->conn->close();
+        }
+
+        // checks the user credentials against the database, and returns true if they match.
+        function checkLogin($username, $password) {
+            $dbusername = ""; $pwhash = "";
+
+            $stmt = $this->conn->prepare("SELECT `password` FROM users WHERE username = ?");
+            if ($stmt == true) {
+                $stmt->bind_param("s", $username);
+                $stmt->execute();
+                $stmt->bind_result($pwhash);
+                $stmt->fetch();
+
+                $verify = password_verify($password, $pwhash);
+                if ($verify == true) {
+                    // user credentials match
+                    return true;
+                }
+            }
+            // user credentials don't match, or there is an error.
+            return false;
+        }
+
+        // gets the userid based on the username parameter, returns -1 on error.
+        function getUserID($username) {
+            $userid = -1;
+            $stmt = $this->conn->prepare("SELECT `id` FROM users WHERE username = ?");
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $stmt->bind_result($userid);
+            $stmt->fetch();
+
+            return $userid;
+        }
+
+        // gets an array of the user's events that they have created.
+        function getUserEvents($userid) {
+            $stmt = $this->conn->prepare("SELECT * FROM events WHERE userid=?");
+            $stmt->bind_param("s", $userid);
+            $stmt->execute();
+            $stmt->store_result();
+            $stmt->bind_result($a,$b,$c,$d,$e,$f,$g,$h,$i,$j,$k,$l);
+            $events = [];
+            if ($stmt && ($stmt->num_rows >= 1)) {
+                while ($stmt->fetch()) {
+                    $events[] = ["id" => $a, "name" => $c, "org" => $d, "sdate" => $e, "edate" => $f];
+                }
+            }
+
+            return $events;
+        }
+    }
+?>
