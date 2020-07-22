009b91210ec7e360d8b613d4e69c57d0ef6ae496
Tim Stegall <47236753+tim-steg@users.noreply.github.com>
2020-07-22 04:30:35 -0500

    Create deleteevent.php

---
+++ deleteevent.php
@@ -0,0 +1,18 @@
+<?php 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    session_start();
+
+    if (isset($_SESSION['authuser'])) {
+        if ($_GET['confirm'] == true) {
+            $dbcon = new dbConnect();
+            $dbcon->connectToDB();
+
+            $dbcon->deleteEvent($_GET['id']);
+            header("Location: ./my-events.php?id=".$_SESSION['userid']);
+        }
+    } else {
+        header("Location: ./index.php");
+    }
+?>
